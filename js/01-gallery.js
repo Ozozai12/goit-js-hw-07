d5e337dc11426a85e4af50561f088fe3de72dbbf
Artem Karpenko <dante120591@gmail.com>
2022-07-27 23:27:30 +0300

    Виконує домашку

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,55 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const gallery = document.querySelector('.gallery');
 
-console.log(galleryItems);
+const galleryList = galleryItems.map(item => {
+    return `
+    <div class="gallery__item">
+        <a class="gallery__link" href="${item.original}">
+            <img
+            class="gallery__image"
+            src="${item.preview}"
+            data-source="${item.original}"
+            alt="${item.description}";
+            />
+        </a>
+    </div>
+    `
+}).join("");
+    
+gallery.insertAdjacentHTML('beforeend', galleryList);
+
+gallery.addEventListener('click', onGalleryClick);
+
+function onGalleryClick(event) {
+    event.preventDefault();
+
+    if (event.target.nodeName !== 'IMG') {
+        return;
+    }
+    
+    modalShow(event);
+}
+
+function modalShow(event) {
+    window.addEventListener("keydown", onCloseModalEscape);
+
+    const modalWindow = basicLightbox.create(`
+        <div class="modal">
+            <img src="${event.target.dataset.source}"
+        </div>
+        `)
+    
+    modalWindow.show()
+
+    function onCloseModalEscape(event) {
+       if (event.code === 'Escape') {
+           modalWindow.close();
+           console.log(event)
+           window.removeEventListener("keydown", onCloseModalEscape);
+        } 
+    }
+}
+
+
+    
